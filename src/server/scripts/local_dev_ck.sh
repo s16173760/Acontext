0c4764d806671cc5aaacd9eb9d3b036cdacc7b3a
Gus <jianbaiye@outlook.com>
2025-07-21 17:14:45 +0800

    chore: add ck image

---
+++ src/server/scripts/local_dev_ck.sh
@@ -0,0 +1,8 @@
+set -e
+rm -rf ./db/dev_data
+rm -rf ./db/dev_redis
+rm -rf ./db/dev_clickhouse
+DATABASE_LOCATION="./db/dev_data" REDIS_LOCATION="./db/dev_redis" CLICKHOUSE_LOCATION="./db/dev_clickhouse" docker compose up acontext-server-clickhouse
+rm -rf ./db/dev_data
+rm -rf ./db/dev_redis
+rm -rf ./db/dev_clickhouse
